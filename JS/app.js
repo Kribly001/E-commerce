30efeecf7c72af1fadfdbae8c585e1a80d0a71b2
Maxi Centeno <Contactmaxi531@gmail.com>
2022-07-05 10:49:10 -0300

    Actualizacion

--- JS/app.js
+++ JS/app.js
@@ -1,22 +1,22 @@
-//   let namevisit = prompt("Ingresa tu nombre");
-//   while (namevisit === "") {
-//       alert("No ingreso su nombre");
-//       namevisit = prompt("Ingresa tu nombre");
-//   }
-//   alert("Bienvenido " + namevisit + " disfruta tu estadia aqui, para saber el precio del producto presiona la imagen");
+  let namevisit = prompt("Ingresa tu nombre");
+  while (namevisit === "") {
+      alert("No ingreso su nombre");
+      namevisit = prompt("Ingresa tu nombre");
+  }
+  alert("Bienvenido " + namevisit + " disfruta tu estadia aqui, para saber el precio del producto presiona la imagen");
 
-//   function saberprecio(number) {
-//       let valor;
-//       let compra = prompt("Su precio es $" + number + " desea compra el producto? SI o NO");
-//       if (compra == "SI") {
-//           alert("Compra finalizada pronto le llegara por correo")
-//       } else if (compra == "NO") {
-//           alert("No se completo la compra")
-//       } else {
-//           alert("No se ingreso un valor validoo");
-//       }
-//       return compra;
-//   }
+  function saberprecio(number) {
+      let valor;
+      let compra = prompt("Su precio es $" + number + " desea agregar al carrito? SI o NO");
+      if (compra == "SI") {
+          alert("Compra finalizada pronto le llegara por correo")
+      } else if (compra == "NO") {
+          alert("No se completo la compra")
+      } else {
+          alert("No se ingreso un valor validoo");
+      }
+      return compra;
+  }
 
 
 
