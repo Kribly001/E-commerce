46726bbf6dd9979c51138301a1625d940df3b91d
Maxi Centeno <Contactmaxi531@gmail.com>
2022-07-05 03:46:10 -0300

    Actualizacion

--- JS/app.js
+++ JS/app.js
@@ -1,19 +1,29 @@
-let namevisit = prompt("Ingresa tu nombre");
-while (namevisit === "") {
-    alert("No ingreso su nombre");
-    namevisit = prompt("Ingresa tu nombre");
-}
-alert("Bienvenido " + namevisit + " disfruta tu estadia aqui, para saber el precio del producto presiona la imagen");
+  let namevisit = prompt("Ingresa tu nombre");
+  while (namevisit === "") {
+      alert("No ingreso su nombre");
+      namevisit = prompt("Ingresa tu nombre");
+  }
+  alert("Bienvenido " + namevisit + " disfruta tu estadia aqui, para saber el precio del producto presiona la imagen");
 
-function saberprecio(number) {
-    let valor;
-    let compra = prompt("Su precio es $" + number + " desea compra el producto? SI o NO");
-    if (compra == "SI") {
-        alert("Compra finalizada pronto le llegara por correo")
-    } else if (compra == "NO") {
-        alert("No se completo la compra")
-    } else {
-        alert("No se ingreso un valor valido");
-    }
-    return compra;
-}
+  function saberprecio(number) {
+      let valor;
+      let compra = prompt("Su precio es $" + number + " desea compra el producto? SI o NO");
+      if (compra == "SI") {
+          alert("Compra finalizada pronto le llegara por correo")
+      } else if (compra == "NO") {
+          alert("No se completo la compra")
+      } else {
+          alert("No se ingreso un valor valido");
+      }
+      return compra;
+  }
+
+
+
+let product = document.getElementsByClassName("nameproduct")
+console.log(product[0].innerHTML);
+console.log(product[1].innerHTML);
+console.log(product[2].innerHTML);
+console.log(product[3].innerHTML);
+console.log(product[4].innerHTML);
+console.log(product[5].innerHTML);
