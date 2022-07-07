f8ae883e6d5d8473129456e9ccf9c67abf5461b7
Maxi Centeno <Contactmaxi531@gmail.com>
2022-07-07 17:36:01 -0300

    Carrito Agregado

--- JS/app.js
+++ JS/app.js
@@ -1,29 +1,75 @@
-  let namevisit = prompt("Ingresa tu nombre");
-  while (namevisit === "") {
-      alert("No ingreso su nombre");
-      namevisit = prompt("Ingresa tu nombre");
-  }
-  alert("Bienvenido " + namevisit + " disfruta tu estadia aqui, para saber el precio del producto presiona la imagen");
+const addToShoppingCartButtons = document.querySelectorAll('.addToCart');
+addToShoppingCartButtons.forEach((addToCartButton) => {
+    addToCartButton.addEventListener('click', addToCartClicked);
+});
 
-  function saberprecio(number) {
-      let valor;
-      let compra = prompt("Su precio es $" + number + " desea agregar al carrito? SI o NO");
-      if (compra == "SI") {
-          alert("Compra finalizada pronto le llegara por correo")
-      } else if (compra == "NO") {
-          alert("No se completo la compra")
-      } else {
-          alert("No se ingreso un valor validoo");
-      }
-      return compra;
-  }
+const shoppingCartItemsContainer = document.querySelector(
+    '.shoppingCartItemsContainer'
+);
 
+function addToCartClicked(event) {
+    const button = event.target;
+    const item = button.closest('.item');
 
+    const itemTitle = item.querySelector('.item-title').textContent;
+    const itemPrice = item.querySelector('.item-price').textContent;
+    const itemImage = item.querySelector('.item-image').src;
 
-let product = document.getElementsByClassName("nameproduct")
-console.log(product[0].innerHTML);
-console.log(product[1].innerHTML);
-console.log(product[2].innerHTML);
-console.log(product[3].innerHTML);
-console.log(product[4].innerHTML);
-console.log(product[5].innerHTML);
+    addItemToShoppingCart(itemTitle, itemPrice, itemImage);
+}
+
+function addItemToShoppingCart(itemTitle, itemPrice, itemImage) {
+    const elementsTitle = shoppingCartItemsContainer.getElementsByClassName(
+        'shoppingCartItemTitle'
+    );
+    for (let i = 0; i < elementsTitle.length; i++) {
+        if (elementsTitle[i].innerText === itemTitle) {
+            let elementQuantity = elementsTitle[
+                i
+            ].parentElement.parentElement.parentElement.querySelector(
+                '.shoppingCartItemQuantity'
+            );
+            elementQuantity.value++;
+            $('.toast').toast('show');
+            updateShoppingCartTotal();
+            return;
+        }
+    }
+
+    const shoppingCartRow = document.createElement('div');
+    const shoppingCartContent = `
+    <div class="row shoppingCartItem">
+          <div class="col-6">
+              <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                  <img height="150" width="150" src=${itemImage} class="shopping-cart-image">
+                  <h6 class=" textCarrito shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemTitle}</h6>
+              </div>
+          </div>
+          <div class="col-2">
+              <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                  <p class="textCarrito item-price mb-0 shoppingCartItemPrice">${itemPrice}</p>
+              </div>
+          </div>
+          <div class="col-4">
+              <div
+                  class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
+                  <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
+                      value="1">
+                  <button class="btn btn-danger buttonDelete" type="button">X</button>
+              </div>
+          </div>
+      </div>`;
+    shoppingCartRow.innerHTML = shoppingCartContent;
+    shoppingCartItemsContainer.append(shoppingCartRow);
+
+    updateShoppingCartTotal();
+}
+
+function updateShoppingCartTotal() {
+    let total = 0;
+    const shoppingCartTotal = document.querySelector('.shoppingCartTotal')
+    const shoppingCartItems = document.querySelectorAll('shoppingCartItem')
+    shoppingCartItems.forEach(shoppingCartItems => {
+
+    })
+}
